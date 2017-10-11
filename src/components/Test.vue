34fc5fafbc3b9f3289919c196385d33e87bbc2fd
coolworld2015 <wintermute2015@ukr.net>
2017-10-11 23:38:23 +0300

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="wrapper">
+    <navbar></navbar>
+	<div class="content">
+	  <main>
+	    <section class="search-results">
+          <div style="position: relative; top: 200px; -webkit-box-align:center; -webkit-box-pack:center; display:-webkit-box; font-size:54px">
+            Касова книга
+          </div>
+        </section>
+      </main>
+	</div></div>
+</template>
+
+<script>
+import navbar from '@/components/Navbar'
+
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'test'
+    }
+  },
+	components: {
+		navbar
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+require('./assets/css/bootstrap.css')
+// require('./assets/css/font-awesome.min.css')
+require('./assets/css/style.css')
+
+Vue.config.productionTip = false
+const appConfig = new Vue();
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
